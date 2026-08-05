ca06a424dac071c751141a800a1a64f9123614f3
Varnikainwueue <varnika.mann15@gmail.com>
2026-08-05 23:32:07 +0530

    File Added

---
+++ Index.java
@@ -0,0 +1,17 @@
+public class Index{
+
+    static int sum(int n) {
+        int s = 0;
+
+        while (n > 0) {
+            s += n % 10;
+            n /= 10;
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(12345));
+    }
+}
